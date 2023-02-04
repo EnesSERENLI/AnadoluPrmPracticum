1d476b1b7d729eae37a35d4e951192d0c4f0e707
EnesSERENLI <enes.serenli@hotmail.com>
2023-02-04 21:36:10 +0300

    Swagger ayarları yapıldı..

--- AnadoluPrmPracticum/Program.cs
+++ AnadoluPrmPracticum/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,7 +7,26 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("HCAPI", new OpenApiInfo()
+    {
+        Title = "RESTful API",
+        Version = "V1",
+        Description = "Restful Api Trials",
+        Contact = new OpenApiContact()
+        {
+            Email = "enes.serenli@hotmail.com",
+            Name = "Enes Serenli",
+            Url = new Uri("https://github.com/EnesSERENLI/AnadoluPrmPracticum")
+        },
+        License = new OpenApiLicense()
+        {
+            Name = "MIT Licence",
+            Url = new Uri("https://opensource.org/licenses/MIT")
+        }
+    });
+});
 
 var app = builder.Build();
 
@@ -13,7 +34,10 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/HCAPI/swagger.json", "HC API");
+    });
 }
 
 app.UseHttpsRedirection();
