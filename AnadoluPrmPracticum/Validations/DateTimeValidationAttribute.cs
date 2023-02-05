f7a08d972afac9af67dde4f72cadf00b861ba8c3
EnesSERENLI <enes.serenli@hotmail.com>
2023-02-05 12:12:11 +0300

    Custom Validaiton Attirbute'ler eklendi.

---
+++ AnadoluPrmPracticum/Validations/DateTimeValidationAttribute.cs
@@ -0,0 +1,23 @@
+﻿using AnadoluPrmPracticum.Entities;
+using System.ComponentModel.DataAnnotations;
+
+namespace AnadoluPrmPracticum.Validations
+{
+    public class DateTimeValidationAttribute : ValidationAttribute
+    {
+        public DateTimeValidationAttribute()
+        {
+            MaxRelaseDate = DateTime.UtcNow.Date;
+        }
+        public DateTime MaxRelaseDate { get; set; }
+
+        public string GetErrorMessage() => $"Oyunun çıkış tarihi şuan ki tarihten büyük olamaz. !!!!!";
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var game = (Game)validationContext.ObjectInstance;
+            var dateIsValid = game.ReleaseDate <= MaxRelaseDate; 
+
+            return dateIsValid ? ValidationResult.Success : new ValidationResult(GetErrorMessage());
+        }
+    }
+}
